46c7ba179741d2ea70aaa75d1efd4373e45dc3d0
Ricki70 <96478796+Ricki70@users.noreply.github.com>
2023-07-21 17:38:09 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,80 +1,120 @@
-document.addEventListener("DOMContentLoaded", function() {
-    const galleryImgs = document.querySelectorAll(".gallery-img");
-    const overlay = document.createElement("div");
-    overlay.classList.add("overlay");
-    document.body.appendChild(overlay);
+ // Obtener referencia al botón de menú
+ const menuButton = document.querySelector('.navbar-toggler');
 
-    const videoThumbnails = document.querySelectorAll(".video-thumbnail");
-    const videoModal = document.getElementById("videoModal");
-    const videoFrame = document.getElementById("videoFrame");
-    const closeBtn = document.querySelector(".close");
-  
-    videoThumbnails.forEach((thumbnail) => {
-      thumbnail.addEventListener("click", function() {
-        const videoSrc = this.dataset.video;
-        openVideoModal(videoSrc);
-      });
-  
-    galleryImgs.forEach((img, index) => {
-      img.addEventListener("click", function() {
-        showEnlargedImage(index);
-      });
+ // Obtener referencia a todos los enlaces dentro del menú
+ const menuLinks = document.querySelectorAll('.navbar-nav a');
+
+ // Obtener referencia al offcanvas
+ const offcanvas = new bootstrap.Offcanvas(document.getElementById('offcanvasDarkNavbar'));
+
+ // Función para cerrar el menú
+ function closeMenu() {
+     offcanvas.hide();
+ }
+
+ // Agregar el evento clic a cada enlace del menú
+ menuLinks.forEach(link => {
+     link.addEventListener('click', closeMenu);
+ });
+
+document.addEventListener("DOMContentLoaded", function () {
+  const galleryImgs = document.querySelectorAll(".gallery-img");
+  const overlay = document.createElement("div");
+  overlay.classList.add("overlay");
+  document.body.appendChild(overlay);
+
+  galleryImgs.forEach((img, index) => {
+    img.addEventListener("click", function () {
+      showEnlargedImage(index);
     });
-  
-    function showEnlargedImage(index) {
-      overlay.style.display = "block";
-      const enlargedImg = document.createElement("img");
-      enlargedImg.classList.add("enlarged-img");
+  });
+
+  function showEnlargedImage(index) {
+    overlay.style.display = "block";
+    const enlargedImg = document.createElement("img");
+    enlargedImg.classList.add("enlarged-img");
+    enlargedImg.src = galleryImgs[index].src;
+    overlay.appendChild(enlargedImg);
+
+    const leftArrow = document.createElement("span");
+    leftArrow.classList.add("carousel-control", "left");
+    leftArrow.innerHTML = "&lt;";
+    leftArrow.addEventListener("click", function () {
+      index = (index - 1 + galleryImgs.length) % galleryImgs.length;
       enlargedImg.src = galleryImgs[index].src;
-      overlay.appendChild(enlargedImg);
-  
-      const leftArrow = document.createElement("span");
-      leftArrow.classList.add("carousel-control", "left");
-      leftArrow.innerHTML = "&lt;";
-      leftArrow.addEventListener("click", function() {
-        index = (index - 1 + galleryImgs.length) % galleryImgs.length;
-        enlargedImg.src = galleryImgs[index].src;
-      });
-      overlay.appendChild(leftArrow);
-  
-      const rightArrow = document.createElement("span");
-      rightArrow.classList.add("carousel-control", "right");
-      rightArrow.innerHTML = "&gt;";
-      rightArrow.addEventListener("click", function() {
-        index = (index + 1) % galleryImgs.length;
-        enlargedImg.src = galleryImgs[index].src;
-      });
-      overlay.appendChild(rightArrow);
+    });
+    overlay.appendChild(leftArrow);
+
+    const rightArrow = document.createElement("span");
+    rightArrow.classList.add("carousel-control", "right");
+    rightArrow.innerHTML = "&gt;";
+    rightArrow.addEventListener("click", function () {
+      index = (index + 1) % galleryImgs.length;
+      enlargedImg.src = galleryImgs[index].src;
+    });
+    overlay.appendChild(rightArrow);
+  }
+
+  overlay.addEventListener("click", function (e) {
+    if (e.target === this) {
+      this.style.display = "none";
+      this.innerHTML = "";
     }
-  
-    overlay.addEventListener("click", function(e) {
-      if (e.target === this) {
-        this.style.display = "none";
-        this.innerHTML = "";
-      }
-    });
   });
+});
+
+// Función para abrir el modal con el video de YouTube embebido en el iframe
+function openVideoModal(videoId) {
+  var videoUrl = "https://www.youtube.com/embed/" + videoId;
+  var iframe = document.getElementById("videoFrame");
+  iframe.setAttribute("src", videoUrl);
+
+  var btncerrar = document.querySelector(".close");
+  var videoContainer = document.querySelector(".modal-content");
+  var videoAspectRatio = 16 / 9;
+
+  var windowAspectRatio = window.innerWidth / window.innerHeight;
+
+  if (Math.abs(windowAspectRatio - videoAspectRatio) < 0.3) {
+    videoContainer.classList.add("with-margin");
+    btncerrar.classList.add("with-margin");
+  } else {
+    videoContainer.classList.remove("with-margin");
+    btncerrar.classList.remove("with-margin");
+  }
+
+  var modal = document.getElementById("videoModal");
+  modal.style.display = "block";
+}
 
 
+// Función para cerrar el modal
+function closeVideoModal() {
+  var modal = document.getElementById("videoModal");
+  var videoFrame = document.getElementById("videoFrame");
 
+  // Detener la reproducción del video
+  videoFrame.src = "";
+  modal.style.display = "none";
+}
 
-  
-    function openVideoModal(src) {
-      videoFrame.querySelector("source").src = src;
-      videoFrame.load(); // Recargar el video para actualizar la fuente
-      videoModal.style.display = "block";
-    }
-  
-    closeBtn.addEventListener("click", function() {
-        alert("prueba");
-      videoFrame.pause(); // Pausar el video al cerrar la modal
-      videoModal.style.display = "none";
-    });
-  
-    window.addEventListener("click", function(event) {
-      if (event.target === videoModal) {
-        videoFrame.pause(); // Pausar el video al hacer clic fuera de la modal
-        videoModal.style.display = "none";
-      }
-    });
+// Event listener para las miniaturas de video
+var thumbnails = document.getElementsByClassName("video-thumbnail");
+for (var i = 0; i < thumbnails.length; i++) {
+  thumbnails[i].addEventListener("click", function () {
+    var videoId = this.getAttribute("data-video");
+    openVideoModal(videoId);
   });
+}
+
+// Event listener para cerrar el modal
+var closeButton = document.getElementsByClassName("close")[0];
+closeButton.addEventListener("click", closeVideoModal);
+
+// Event listener para cerrar el modal al hacer clic fuera del video
+window.addEventListener("click", function (event) {
+  var modal = document.getElementById("videoModal");
+  if (event.target == modal) {
+    closeVideoModal();
+  }
+});
